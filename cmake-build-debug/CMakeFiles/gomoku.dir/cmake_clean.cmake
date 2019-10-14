62fc5dc96c1ac5f5aeddcc0b2c5393497534d810
piotr <ptl99t@gmail.com>
2019-10-14 20:42:22 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/gomoku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gomoku.dir/main.cpp.o"
+  "gomoku.pdb"
+  "gomoku"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gomoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
